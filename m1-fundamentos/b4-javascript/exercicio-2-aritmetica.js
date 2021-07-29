fe7a44677c89600225d29db1c096de57733d6ff0
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-07-29 13:38:24 -0300

    Adiciona exercicio 4.2

---
+++ m1-fundamentos/b4-javascript/exercicio-2-aritmetica.js
@@ -0,0 +1,26 @@
+/**
+ * * Tipos primitivos
+ * A tipagem do JavaScript é dinâmica. É possível redefinir o valor de uma variável no futuro.
+ */
+
+let movie = "The Lord of the Rings"; //  string literal
+let score = 10.89; //  number literal
+let isValid = true; //  boolean
+let name; //  undefined
+let color = null; //  nulo
+color = "red"; // redefinição
+
+/**
+ * *Aritmética
+ * !    +       -             *          /        **
+ *    soma  subtracao  multiplicacao  divisao  potencia
+ */
+
+// ? Exercício 1
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: "Ana",
+  lastName: "Santos",
+};
+const patientEmail = "ana@email.com";
