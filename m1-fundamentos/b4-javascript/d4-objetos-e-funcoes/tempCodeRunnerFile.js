1c0176e49f632661823b74a6a07c54573a8438f1
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-08-03 14:01:26 -0300

    Adiciona conceito do exercicio 7

--- m1-fundamentos/b4-javascript/d4-objetos-e-funcoes/tempCodeRunnerFile.js
+++ m1-fundamentos/b4-javascript/d4-objetos-e-funcoes/tempCodeRunnerFile.js
@@ -1,13 +1,13 @@
-let word = "trybe";
-let ending = "be";
+let word = "banana"; // exemplo com as palavras trybe e be
+let ending = "nana";
 
-let wordSplit = word.split("");
-let endingSplit = ending.split("");
+let wordSplit = word.split(""); // divide "trybe" em letras --> ['t','r','y','b','e']
+let endingSplit = ending.split(""); // divide "be" em letras --> ['b','e']
 
-let slicer = -1 * endingSplit.length;
-let wordEnding = wordSplit.slice(slicer).join("");
+let slicer = -1 * endingSplit.length; // vou cortar o array wordSplit do tamanho do array endingSplit, multiplico por -1 para cortar do final para o início, já que queremos comparar o final
+let wordEnding = wordSplit.slice(slicer).join(""); // corto o array wordSplit e uno o resultado em uma única string
 
-if (wordEnding === ending) {
+if (wordEnding === ending) { // comparação entre a string recém feita e o ending inicial
   console.log(true);
 } else {
   console.log(false);
