bd394814101b2bf7d1878962b1575de4b8d7ac0a
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-08-27 19:06:56 -0300

    Adiciona exercicio 3 da parte 2 - Cria uma pagina que conta os cliques no botao

--- m1-fundamentos/b7-ES6-testes-unitarios/parte2.js
+++ m1-fundamentos/b7-ES6-testes-unitarios/parte2.js
@@ -28,3 +28,19 @@ function findLongestWord(str) {
   console.log(max);
 }
 findLongestWord('Antônio foi no banheiro e não sabemos o que aconteceu');
+
+/**
+ * * Exercício 3 - Criar uma página que conte os cliques
+ */
+const button = document.getElementById('btn');
+button.textContent = 'Contador: 0';
+function handleClick() {
+  let clickCount = 0;
+
+  button.addEventListener('click', (event) => {
+    const element = event.target;
+    clickCount += 1;
+    element.textContent = `Contador: ${clickCount}`;
+  });
+}
+handleClick();
