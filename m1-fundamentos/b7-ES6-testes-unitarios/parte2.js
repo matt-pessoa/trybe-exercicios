602f6269bf5b18562c3a21d4b109b3f0b5b4e2a6
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-08-27 18:54:20 -0300

    Adiciona exercicio 2 da parte 2 - Criar uma funçao retorne a maior palavra de uma string

--- m1-fundamentos/b7-ES6-testes-unitarios/parte2.js
+++ m1-fundamentos/b7-ES6-testes-unitarios/parte2.js
@@ -4,9 +4,27 @@
 
 function calculateFactorial(num) {
   let factorial = num;
-  for (let nextNumber = (num-1); nextNumber >= 1; nextNumber -= 1){
+  for (let nextNumber = num - 1; nextNumber >= 1; nextNumber -= 1) {
     factorial *= nextNumber;
   }
   return factorial;
 }
 console.log(calculateFactorial(5));
+
+// Fatorial utilizando ternary operator & arrow function
+const factorialTernary = (num) =>
+  num > 1 ? num * factorialTernary(num - 1) : 1;
+console.log(factorialTernary(5));
+
+/**
+ * * Exercício 2 - Crie uma função que retorne qual a maior palavra
+ */
+function findLongestWord(str) {
+  strSplit = str.split(' ');
+  max = strSplit[0];
+  for (let word of strSplit) {
+    word.length > max.length ? (max = word) : (max = max); // word é maior que max? se sim, max = word, se não max é max.
+  }
+  console.log(max);
+}
+findLongestWord('Antônio foi no banheiro e não sabemos o que aconteceu');
