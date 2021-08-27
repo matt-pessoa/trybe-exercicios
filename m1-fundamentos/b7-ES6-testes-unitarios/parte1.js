bb3bcd8dae792b9ca71ee3dab0994b727aedd250
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-08-27 18:15:22 -0300

    Adiciona exercicio 2 da parte I

--- m1-fundamentos/b7-ES6-testes-unitarios/parte1.js
+++ m1-fundamentos/b7-ES6-testes-unitarios/parte1.js
@@ -1,3 +1,6 @@
+/**
+ * * Exercício 1
+ */
 const testingScope = (escopo) => {
   if (escopo === true) {
     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -11,3 +14,32 @@ const testingScope = (escopo) => {
 }
 
 testingScope(true);
+
+/**
+ * * Exercício 2
+ */
+ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+ // Seu código aqui.
+ oddsAndEvens.sort((a,b) => {
+   return a - b;
+ });
+ 
+ console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
+ 
+ /**
+  * ! .sort([funcaoDeComparacao])
+  * ? Para entender a função de comparação: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+  * 
+  * * 1) funcaoDeComparacao omitida:
+  *      o array é ordenado de acordo com a pontução de código Unicode de cada um dos caracteres     (ordena de acordo com sua corversao para texto).
+  *  ex: 9 vem antes de 80 numericamente, mas em texto "80" vem antes de "9".
+  * 
+  * * 2) funcaoDeComparacao 
+  *    * a) .sort() retorna < 0:
+  *        ordena A para um índice anterior a B. A vem primeiro.
+  *    * b) .sort() retorna === 0:
+  *        ordenacao inalterada.
+  *    * c) .sort() retorna > 0:
+  *        ordena B para um índice anterior a A. B vem primeiro.
+  */ 
