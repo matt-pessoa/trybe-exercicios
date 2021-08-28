183390c8b388c41e965db3310d42cf23bf60dc62
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-08-28 12:54:13 -0300

    Reorganiza exercicios em diferentes arquivos

---
+++ m1-fundamentos/b7-ES6-testes-unitarios/pt1-exercicio-2.js
@@ -0,0 +1,30 @@
+/**
+ * * Exercício 2
+ */
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+oddsAndEvens.sort((a, b) => {
+  return a - b;
+});
+
+console.log(
+  `Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`
+);
+
+/**
+ * ! .sort([funcaoDeComparacao])
+ * ? Para entender a função de comparação: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+ *
+ * * 1) funcaoDeComparacao omitida:
+ *      o array é ordenado de acordo com a pontução de código Unicode de cada um dos caracteres     (ordena de acordo com sua corversao para texto).
+ *  ex: 9 vem antes de 80 numericamente, mas em texto "80" vem antes de "9".
+ *
+ * * 2) funcaoDeComparacao
+ *    * a) .sort() retorna < 0:
+ *        ordena A para um índice anterior a B. A vem primeiro.
+ *    * b) .sort() retorna === 0:
+ *        ordenacao inalterada.
+ *    * c) .sort() retorna > 0:
+ *        ordena B para um índice anterior a A. B vem primeiro.
+ */
--- m1-fundamentos/b7-ES6-testes-unitarios/pt2-exercicio-1.js
+++ m1-fundamentos/b7-ES6-testes-unitarios/pt2-exercicio-1.js
@@ -31,32 +31,3 @@ console.log(recursiveFactorial(5));
 const factorialTernary = (num) =>
   num > 1 ? num * factorialTernary(num - 1) : 1;
 console.log(factorialTernary(5));
-
-/**
- * * Exercício 2 - Crie uma função que retorne qual a maior palavra
- */
-function findLongestWord(str) {
-  strSplit = str.split(' ');
-  max = strSplit[0];
-  for (let word of strSplit) {
-    word.length > max.length ? (max = word) : (max = max); // word é maior que max? se sim, max = word, se não max é max.
-  }
-  console.log(max);
-}
-findLongestWord('Antônio foi no banheiro e não sabemos o que aconteceu');
-
-/**
- * * Exercício 3 - Criar uma página que conte os cliques
- */
-const button = document.getElementById('btn');
-button.textContent = 'Contador: 0';
-function handleClick() {
-  let clickCount = 0;
-
-  button.addEventListener('click', (event) => {
-    const element = event.target;
-    clickCount += 1;
-    element.textContent = `Contador: ${clickCount}`;
-  });
-}
-handleClick();
