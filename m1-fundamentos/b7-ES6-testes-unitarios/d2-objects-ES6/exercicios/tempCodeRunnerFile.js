b0f2f747ad4191ffd575bbd9ed36756cc773e85f
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-02 13:12:48 -0300

    Inicia exercicios da parte 2

--- m1-fundamentos/b7-ES6-testes-unitarios/d2-objects-ES6/exercicios/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-
-// const orderModifier = (order) => {
-//   // Adicione abaixo as informações necessárias.
-//   const message = customerInfo();
-
-//   console.log(message);
-// };
-
-// orderModifier(order);
