0274235c730d40146af9456fb19c0b4f757ca46d
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-03 13:11:58 -0300

    Exercicio 3 - Teste 3: Verifica se o array passado como parametro sofreu alteraçoes

--- m1-fundamentos/b7-ES6-testes-unitarios/d3-testes-unitarios/parte-1-exercicio-3.js
+++ m1-fundamentos/b7-ES6-testes-unitarios/d3-testes-unitarios/parte-1-exercicio-3.js
@@ -18,3 +18,8 @@ assert.deepStrictEqual([1, 2, 4], myRemoveWithoutCopy([1, 2, 3, 4], 3));
 
 // Teste 2
 assert.notDeepStrictEqual([1, 2, 3, 4], myRemoveWithoutCopy([1, 2, 3, 4], 3));
+
+// Teste 3
+const myArray = [5, 6, 7, 8];
+myRemoveWithoutCopy(myArray, 7);
+assert.strictEqual(myArray, [5, 6, 7, 8]);
