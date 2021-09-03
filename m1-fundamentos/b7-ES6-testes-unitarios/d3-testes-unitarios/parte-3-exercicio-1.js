b0aa641682cb186f3aaf52d36aeedc3b7d57ccf9
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-03 14:15:00 -0300

    Exercicio 1 - Teste 3: corrige o output e verifica se o output === array

--- m1-fundamentos/b7-ES6-testes-unitarios/d3-testes-unitarios/parte-3-exercicio-1.js
+++ m1-fundamentos/b7-ES6-testes-unitarios/d3-testes-unitarios/parte-3-exercicio-1.js
@@ -1,10 +1,12 @@
 const greetPeople = (people) => {
   let greeting = 'Hello ';
+  let newArray = [];
 
   for (const person in people) {
     greeting += people[person];
+    newArray.push(greeting);
   }
-  return greeting;
+  return newArray;
 };
 
 const parameter = ['Irina', 'Ashleigh', 'Elsa'];
@@ -16,4 +18,5 @@ const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
 const assert = require('assert');
 
 assert.strictEqual(typeof greetPeople, 'function');
-assert.deepStrictEqual(greetPeople(parameter), result); // actual: 'Hello IrinaAshleighElsa --> Não é um array, não tem um 'Hello' para cada nome
+// assert.deepStrictEqual(greetPeople(parameter), result); // actual: 'Hello IrinaAshleighElsa --> Não é um array, não tem um 'Hello' para cada nome
+assert.strictEqual(true, Array.isArray(parameter));
