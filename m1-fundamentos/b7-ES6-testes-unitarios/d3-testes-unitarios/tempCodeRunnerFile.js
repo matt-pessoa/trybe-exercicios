d1b6e4b4b63f3faf96c139ceb02a02a9a1a4465b
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-03 14:20:46 -0300

    Exercicio 1 - Teste 4: corrige a concatenacao dos nomes e testa se o output === resultado

--- m1-fundamentos/b7-ES6-testes-unitarios/d3-testes-unitarios/tempCodeRunnerFile.js
+++ m1-fundamentos/b7-ES6-testes-unitarios/d3-testes-unitarios/tempCodeRunnerFile.js
@@ -1,2 +1,14 @@
+const greetPeople = (people) => {
+  let greeting = '';
+  let newArray = [];
+
+  for (const person in people) {
+    greeting = `Hello ${people[person]}`;
+    newArray.push(greeting);
+  }
+  return newArray;
+};
+
 const parameter = ['Irina', 'Ashleigh', 'Elsa'];
-console.log(typeof parameter);
+const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
+console.log(greetPeople(parameter));
