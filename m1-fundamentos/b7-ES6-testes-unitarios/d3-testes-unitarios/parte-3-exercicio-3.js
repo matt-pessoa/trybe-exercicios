83a45dc4729e4945401abefcd822934e9a7a4ece
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-03 14:58:05 -0300

    Exercicio 3 - Teste 2: Verifica se o expected === actual && diagnostico dos erros na funçao

--- m1-fundamentos/b7-ES6-testes-unitarios/d3-testes-unitarios/parte-3-exercicio-3.js
+++ m1-fundamentos/b7-ES6-testes-unitarios/d3-testes-unitarios/parte-3-exercicio-3.js
@@ -1,3 +1,8 @@
+/**
+ * ! A função possui os seguintes erros:
+ * todo expected: output ser array // actual: number
+ * todo expected: array com numeros > 10 // actual: soma dos numeros maiores que 10
+ */
 const greaterThanTen = (array) => {
   let results = 0;
   for (let index = 0; index < array.length; index += 1) {
@@ -16,4 +21,5 @@ const result = [32, 21];
  */
 const assert = require('assert');
 
-assert.strictEqual(typeof greaterThanTen, 'function');
+// assert.strictEqual(typeof greaterThanTen, 'function');
+assert.deepStrictEqual(greaterThanTen(parameter), result);
