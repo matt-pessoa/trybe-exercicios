50be40631145ee097212b469f875fb13a614ff64
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-03 14:54:48 -0300

    Exercicio 3 - Teste 1: Verifica se o tipo da funçao e function

---
+++ m1-fundamentos/b7-ES6-testes-unitarios/d3-testes-unitarios/parte-3-exercicio-3.js
@@ -0,0 +1,19 @@
+const greaterThanTen = (array) => {
+  let results = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] > 10) {
+      results += array[index];
+    }
+  }
+  return results;
+};
+
+const parameter = [4, 10, 32, 9, 21];
+const result = [32, 21];
+
+/**
+ * * Testes
+ */
+const assert = require('assert');
+
+assert.strictEqual(typeof greaterThanTen, 'function');
