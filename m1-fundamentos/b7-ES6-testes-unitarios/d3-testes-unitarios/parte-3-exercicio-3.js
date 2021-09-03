9d4e2f3faf854f2f11ea4a6174992e5d19a993db
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-03 14:59:55 -0300

    Exercicio 3 - Teste 3: Verifica se o output === array

--- m1-fundamentos/b7-ES6-testes-unitarios/d3-testes-unitarios/parte-3-exercicio-3.js
+++ m1-fundamentos/b7-ES6-testes-unitarios/d3-testes-unitarios/parte-3-exercicio-3.js
@@ -1,13 +1,13 @@
 /**
  * ! A função possui os seguintes erros:
- * todo expected: output ser array // actual: number
+ * todo expected: output ser array // actual: number OK
  * todo expected: array com numeros > 10 // actual: soma dos numeros maiores que 10
  */
 const greaterThanTen = (array) => {
-  let results = 0;
+  let results = [];
   for (let index = 0; index < array.length; index += 1) {
     if (array[index] > 10) {
-      results += array[index];
+      results.push(array[index]);
     }
   }
   return results;
@@ -22,4 +22,5 @@ const result = [32, 21];
 const assert = require('assert');
 
 // assert.strictEqual(typeof greaterThanTen, 'function');
-assert.deepStrictEqual(greaterThanTen(parameter), result);
+assert.strictEqual(true, Array.isArray(greaterThanTen(parameter)));
+// assert.deepStrictEqual(greaterThanTen(parameter), result);
