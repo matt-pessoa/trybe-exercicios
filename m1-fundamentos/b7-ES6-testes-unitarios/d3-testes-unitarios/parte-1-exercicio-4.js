79649e1b14607f44da3ac026befe1064dd4534dd
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-03 13:27:36 -0300

    Exercicio 4 - Teste 1: Verifica se o retorno da funçao e o esperado

---
+++ m1-fundamentos/b7-ES6-testes-unitarios/d3-testes-unitarios/parte-1-exercicio-4.js
@@ -0,0 +1,13 @@
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+// implemente seus testes aqui
+const assert = require('assert');
+
+// Teste 1
+assert.strictEqual('fizzbuzz', myFizzBuzz(15));
