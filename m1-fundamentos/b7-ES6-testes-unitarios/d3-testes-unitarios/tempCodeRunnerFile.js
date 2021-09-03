9647cb08af27e976eb486216c9ae154c671fbea1
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-03 13:42:21 -0300

    Exercicio 1 - Cria uma funçao que recebe um array e monta um novo array com todos os numeros somados a 1

--- m1-fundamentos/b7-ES6-testes-unitarios/d3-testes-unitarios/tempCodeRunnerFile.js
+++ m1-fundamentos/b7-ES6-testes-unitarios/d3-testes-unitarios/tempCodeRunnerFile.js
@@ -1,4 +1,2 @@
-// Teste 3
-// const myArray = [5, 6, 7, 8];
-// myRemoveWithoutCopy(myArray, 7);
-// assert.strictEqual(myArray, [5, 6, 7, 8]); // ! O array passado sofreu alterações
+assert.deepStrictEqual(output, expected);
+// assert.deepStrictEqual(myArray, unchanged);
