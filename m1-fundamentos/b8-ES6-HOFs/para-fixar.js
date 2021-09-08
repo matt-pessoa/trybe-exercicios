f8b813c119a01aadcfc9853f8318264230dab652
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-08 15:37:03 -0300

    Para fixar - Exercicio 4 - Cria uma HOF que imprima o resultado da execuçao das outras funçoes

--- m1-fundamentos/b8-ES6-HOFs/para-fixar.js
+++ m1-fundamentos/b8-ES6-HOFs/para-fixar.js
@@ -3,3 +3,9 @@ const wakeUp = () => 'Acordando!!';
 const breakfast = () => 'Bora tomar café!!';
 
 const sleepTime = () => 'Partiu dormir!!';
+
+const doingThings = (func) => console.log(func());
+
+doingThings(wakeUp);
+doingThings(breakfast);
+doingThings(sleepTime);
