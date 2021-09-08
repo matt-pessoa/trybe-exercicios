32197f4c387873590734309c4c936d8c55fef557
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-08 16:34:20 -0300

    [Exercicios] - Exercicio 3: Refatora funçoes para que a finalResult cuide da soma das questoes e a checkAnswers cuide da comparaçao entre as respostas e o gabarito

--- m1-fundamentos/b8-ES6-HOFs/exercicio-3.js
+++ m1-fundamentos/b8-ES6-HOFs/exercicio-3.js
@@ -1,23 +1,26 @@
-const handleGabarito = (studentAnswers, rightAnswers, func) => {
-  return func(studentAnswers, rightAnswers);
-};
-
-const handlePontos = (studentAnswers, rightAnswers) => {
+const handleFinalResult = (studentAnswers, rightAnswers, action) => {
   const testLength = rightAnswers.length;
   let total = 0;
 
   for (let index = 0; index < testLength; index += 1) {
-    if (studentAnswers[index] === 'N.A') {
-      total = total;
-    } else if (studentAnswers[index] === rightAnswers[index]) {
-      total += 1;
-    } else {
-      total -= 0.5;
-    }
+    const questionResult = action(studentAnswers[index], rightAnswers[index]);
+    total += questionResult;
   }
   return total;
 };
 
+const handleCheckAnswers = (studentAnswer, rightAnswer) => {
+  if (studentAnswer === 'N.A') {
+    return 0;
+  } else if (studentAnswer === rightAnswer) {
+    return 1;
+  } else {
+    return -0.5;
+  }
+};
+
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
-console.log(handleGabarito(STUDENT_ANSWERS, RIGHT_ANSWERS, handlePontos));
+console.log(
+  handleFinalResult(STUDENT_ANSWERS, RIGHT_ANSWERS, handleCheckAnswers)
+);
