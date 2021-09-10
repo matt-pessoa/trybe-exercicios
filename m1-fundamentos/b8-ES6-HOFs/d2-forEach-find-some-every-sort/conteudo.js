02eab1e08800da77d9cbcbde48603e1a5a922b89
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-10 01:51:13 -0300

    [Para fixar] .find - Exercicio 1: Utiliza o find para retornar o primeiro numero do array divisivel por 3 e por 5

--- m1-fundamentos/b8-ES6-HOFs/d2-forEach-find-some-every-sort/conteudo.js
+++ m1-fundamentos/b8-ES6-HOFs/d2-forEach-find-some-every-sort/conteudo.js
@@ -1,5 +1,6 @@
 /**
  * ? Para fixar - forEach
+ * * https://codepen.io/pen/?template=LYZPEwV
  */
 const emailListInData = [
   'roberta@email.com',
@@ -14,4 +15,18 @@ const showEmailList = (email) => {
 
 emailListInData.forEach((element) => showEmailList(element));
 
-// Adicione seu código aqui
+/**
+ * ? Para fixar - find
+ */
+
+// ? 1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = (array) => {
+  // Adiciona seu código aqui
+  const verifyDivisibility = (num) => num % 3 === 0 && num % 5 === 0;
+
+  return array.find(verifyDivisibility);
+};
+
+console.log(findDivisibleBy3And5(numbers));
