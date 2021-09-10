891e48a1b608c3dfb6c56e66e1a61a0900b56ef6
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-10 01:38:25 -0300

    [Para fixar] .forEach: Usa o metodo forEach chamando a callback showEmailList para apresentar os emails

---
+++ m1-fundamentos/b8-ES6-HOFs/d2-forEach-find-some-every-sort/conteudo.js
@@ -0,0 +1,17 @@
+/**
+ * ? Para fixar - forEach
+ */
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+emailListInData.forEach((element) => showEmailList(element));
+
+// Adicione seu código aqui
