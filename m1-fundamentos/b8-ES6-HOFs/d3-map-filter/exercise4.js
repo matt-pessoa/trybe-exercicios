790fb2793f9a6dd5c1c315851c7ef4b186e7fcee
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-12 21:37:38 -0300

    [Exercicio 4] - Cria uma funçao que retorna um array ordenado (crescente) dos livros com mais de 60 anos

--- m1-fundamentos/b8-ES6-HOFs/d3-map-filter/exercise4.js
+++ m1-fundamentos/b8-ES6-HOFs/d3-map-filter/exercise4.js
@@ -64,3 +64,38 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+const expectedResult = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+];
+
+function oldBooksOrdered() {
+  // escreva seu código aqui
+  const actualYear = 2021;
+  const olderThan60 = books.filter(
+    (elemento) => actualYear - elemento.releaseYear > 60
+  );
+
+  return olderThan60.sort((a, b) => a.releaseYear - b.releaseYear);
+}
+
+assert.deepStrictEqual(oldBooksOrdered(), expectedResult);
