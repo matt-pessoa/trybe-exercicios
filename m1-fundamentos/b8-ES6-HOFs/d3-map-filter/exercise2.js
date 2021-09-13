b27883f581c187a849979e495ae5eedfdd247e6a
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-12 21:28:54 -0300

    [Exercicio 2] - Cria uma funçao que retorna um array com os objetos com as chaves idade e nome do autor

--- m1-fundamentos/b8-ES6-HOFs/d3-map-filter/exercise2.js
+++ m1-fundamentos/b8-ES6-HOFs/d3-map-filter/exercise2.js
@@ -64,3 +64,41 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+const expectedResult = [
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
+];
+
+function nameAndAge() {
+  // escreva seu código aqui
+  return books
+    .map((elemento) => ({
+      age: elemento.releaseYear - elemento.author.birthYear,
+      author: elemento.author.name,
+    }))
+    .sort((a, b) => a.age - b.age);
+}
+
+assert.deepStrictEqual(nameAndAge(), expectedResult);
