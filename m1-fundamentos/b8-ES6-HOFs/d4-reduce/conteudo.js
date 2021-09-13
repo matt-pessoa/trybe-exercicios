0e3eb9064d86f0ff0c2c12e8e18da3779091bfa5
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-13 13:53:53 -0300

    [Conteudo] - Cria duas funçoes que retornam a soma dos numeros pares de um array. Uma funçao utilizando o filter e o reduce e a outra utilizando apenas o reduce

--- m1-fundamentos/b8-ES6-HOFs/d4-reduce/conteudo.js
+++ m1-fundamentos/b8-ES6-HOFs/d4-reduce/conteudo.js
@@ -1,4 +1,9 @@
-const numbers = [50, 85, -30, 3, 15];
+const assert = require('assert');
+
+const numbers = [18, 19, 23, 53, 4, 5, 85, 76, 23, 54];
+/**
+ * ? 1 - Crie duas funções que retornam o maior valor do array: uma vez utilizando o for e outra utilizando o reduce.
+ */
 
 const findLargestFor = (array) => {
   let largestNumber = array[0];
@@ -9,7 +14,7 @@ const findLargestFor = (array) => {
   }
   return largestNumber;
 };
-console.log(findLargestFor(numbers));
+assert.strictEqual(findLargestFor(numbers), 85);
 
 const findLargestReduce = (array) => {
   const getLargest = array.reduce((pValue, cValue) =>
@@ -18,4 +23,25 @@ const findLargestReduce = (array) => {
 
   return getLargest;
 };
-console.log(findLargestReduce(numbers));
+assert.strictEqual(findLargestReduce(numbers), 85);
+
+/**
+ * ? 2- Crie uma função que some todos os números pares utilizando reduce.
+ */
+const sumEvenNumbersFilterReduce = (array) => {
+  const filterEven = array.filter((number) => number % 2 === 0);
+  const sumOfEven = filterEven.reduce((acc, number) => acc + number, 0);
+
+  return sumOfEven;
+};
+
+const expectedEvenSum = 18 + 4 + 76 + 54;
+assert.strictEqual(sumEvenNumbersFilterReduce(numbers), expectedEvenSum);
+
+const sumEvenNumbersReduce = (array) => {
+  const sumOfEven = (acc, number) => (number % 2 === 0 ? acc + number : acc);
+  const total = array.reduce(sumOfEven, 0);
+
+  return total;
+};
+assert.strictEqual(sumEvenNumbersReduce(numbers), expectedEvenSum);
