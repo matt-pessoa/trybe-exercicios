ec5dbb9571e2353c5bfb397fbac42c1bf5b17c95
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-13 22:51:31 -0300

    [Para fixar] destructuring - Exercicio 2 - Atraves do array destructuring, corrija os valores das variaveis

--- m1-fundamentos/b8-ES6-HOFs/d5-spread-rest-destructuring/fixar-destructuring.js
+++ m1-fundamentos/b8-ES6-HOFs/d5-spread-rest-destructuring/fixar-destructuring.js
@@ -2,7 +2,19 @@ const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
 
 // saudacoes[1](saudacoes[0]); // Olá
 
-// Produza o mesmo resultado acima, porém utilizando array destructuring
+// ? Produza o mesmo resultado acima, porém utilizando array destructuring
 
 const [string, handleGreeting] = saudacoes;
 handleGreeting(string);
+
+/**
+ * ? Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+ */
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+[comida, animal, bebida] = ['arroz', 'gato', 'água'];
+console.log(comida, animal, bebida);
