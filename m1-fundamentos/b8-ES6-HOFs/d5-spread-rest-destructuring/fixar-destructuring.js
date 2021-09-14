789cfe120e074efc9a86d260068cbf21c5177b20
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-13 22:57:58 -0300

    [Para fixar] destructuring - Exercicio 3 - Atraves do array destructuring faça com que existam apenas numeros pares no array

--- m1-fundamentos/b8-ES6-HOFs/d5-spread-rest-destructuring/fixar-destructuring.js
+++ m1-fundamentos/b8-ES6-HOFs/d5-spread-rest-destructuring/fixar-destructuring.js
@@ -7,9 +7,7 @@ const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
 const [string, handleGreeting] = saudacoes;
 handleGreeting(string);
 
-/**
- * ? Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
- */
+// ? Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
 let comida = 'gato';
 let animal = 'água';
 let bebida = 'arroz';
@@ -18,3 +16,9 @@ console.log(comida, animal, bebida); // arroz gato água
 
 [comida, animal, bebida] = ['arroz', 'gato', 'água'];
 console.log(comida, animal, bebida);
+
+// ? Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+[, , , ...numerosPares] = numerosPares; // I'd rather do this: let onlyEven = numerosPares.slice(3);
+
+console.log(numerosPares); // [6, 8, 10, 12];
