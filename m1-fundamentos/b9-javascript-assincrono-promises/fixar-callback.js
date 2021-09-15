a2106716cec5f85aa6b10304d6113fb29b241b35
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-15 16:14:16 -0300

    [Para fixar] callback - Lidando com erros. Adiciona mais um parametro callback a funçao para printar o erro quando a falha acontecer

--- m1-fundamentos/b9-javascript-assincrono-promises/fixar-callback.js
+++ m1-fundamentos/b9-javascript-assincrono-promises/fixar-callback.js
@@ -20,7 +20,7 @@ const getUser = (callback) => {
 assert.strictEqual(getUser(userFullName), 'Hello! My name is Ivan Ivanovich'); // complete a chamada da função de getUser
 assert.strictEqual(getUser(userNationality), 'Ivan is Russian'); // complete a chamada da função de getUser
 
-// ? 2- 2 - No código abaixo você tem a função getUser modificada, que agora funciona de modo assíncrono e imprime dados de uma pessoa qualquer depois de um certo tempo. Complete a função getUser de forma que ela receba um callback para que possa realizar as operações abaixo sobre a pessoa.
+// ? 2- No código abaixo você tem a função getUser modificada, que agora funciona de modo assíncrono e imprime dados de uma pessoa qualquer depois de um certo tempo. Complete a função getUser de forma que ela receba um callback para que possa realizar as operações abaixo sobre a pessoa.
 const userFullName2 = ({ firstName, lastName }) =>
   `Hello! My name is ${firstName} ${lastName}`;
 const userNationality2 = ({ firstName, nationality }) =>
@@ -42,3 +42,37 @@ const getUser2 = (callback) => {
 
 getUser2(userFullName2); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
 getUser2(userNationality2); // deve imprimir "Ivan is Russian" depois de um certo tempo
+
+// ? 3 - A função getCountry abaixo tem aproximadamente 50% de chance de obter com sucesso um país, tendo um callback para poder ser feita qualquer operação sobre o país retornado. Adicione um outro callback para getCountry , de forma que trate a mensagem de erro retornada.
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) =>
+  console.log(`${name}'s currency is the ${currency}`);
+
+const delay2 = (maxMilliseconds = 5000) =>
+  Math.floor(Math.random() * maxMilliseconds);
+
+const printErrorMessage = (error) =>
+  console.log(`Error getting country: ${error}`);
+
+const getCountry = (onSuccess, onFailure) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5;
+    if (didOperationSucceed) {
+      const country = {
+        name: 'Brazil',
+        hdi: 0.759,
+        currency: 'Real',
+      };
+      onSuccess(country);
+    } else {
+      const errorMessage = 'Country could not be found';
+      onFailure(errorMessage);
+    }
+  }, delay2());
+};
+
+// Deve imprimir "Returned country is Brazil" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryName, printErrorMessage);
+
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryCurrency, printErrorMessage);
