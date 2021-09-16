87f5cf960165223f45370ee9039390a9126e362a
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-16 15:26:26 -0300

    [Exercicio 4] - Cria a funçao sendMarsTemperature que loga a temperatura de marte em um tempo que varia de 0 a 5 segundos

---
+++ m1-fundamentos/b9-javascript-assincrono-promises/exercise4.js
@@ -0,0 +1,19 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+
+const sendMarsTemperature = (callback) => {
+  const variableTime = Math.floor(Math.random() * 5000);
+
+  return setTimeout(() => {
+    console.log(callback());
+  }, variableTime);
+};
+
+sendMarsTemperature(getMarsTemperature); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
