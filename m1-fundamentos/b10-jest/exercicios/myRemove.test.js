d4c1fcd6ffb187b2a196ccd8173fda8110194bcc
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-23 13:55:10 -0300

    [Exercicio 2.3] - Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado

--- m1-fundamentos/b10-jest/exercicios/myRemove.test.js
+++ m1-fundamentos/b10-jest/exercicios/myRemove.test.js
@@ -10,4 +10,9 @@ describe('Exercício 2', () => {
     const notExpectedArray = [1, 2, 3, 4];
     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual(notExpectedArray);
   });
+
+  it('Verifica se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    const expectedArray = [1, 2, 3, 4];
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual(expectedArray);
+  });
 });
