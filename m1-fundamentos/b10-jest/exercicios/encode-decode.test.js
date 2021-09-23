63bc6e6b6a8071e271ffd35afac7d90ce3de690f
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-23 14:18:12 -0300

    [Exercicio 4.3] - Verifica, para a funçao decode, se os numeros 1, 2, 3, 4, 5 sao convertidos em a, e, i ,o, u

--- m1-fundamentos/b10-jest/exercicios/encode-decode.test.js
+++ m1-fundamentos/b10-jest/exercicios/encode-decode.test.js
@@ -10,4 +10,8 @@ describe('Exercício 4', () => {
   it('Para a função encode testa se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5', () => {
     expect(encodeDecode.encode('a e i o u')).toMatch('1 2 3 4 5');
   });
+
+  it('Para a função decode testa se os números 1, 2, 3, 4 e 5 são convertidos nas vogais a, e, i, o, u', () => {
+    expect(encodeDecode.decode('1 2 3 4 5')).toMatch('a e i o u');
+  });
 });
