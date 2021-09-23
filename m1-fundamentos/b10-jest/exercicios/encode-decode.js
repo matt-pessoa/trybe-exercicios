cd1241e795546a9b425f028251a5b7f5acf0e892
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-23 14:51:20 -0300

    Refatora as funçoes encode e decode

--- m1-fundamentos/b10-jest/exercicios/encode-decode.js
+++ m1-fundamentos/b10-jest/exercicios/encode-decode.js
@@ -1,41 +1,37 @@
 function encode(str) {
   // seu código aqui
-  let strSplit = str.split('');
-  for (let index in strSplit) {
-    if (strSplit[index] === 'a') {
-      strSplit[index] = 1;
-    } else if (strSplit[index] === 'e') {
-      strSplit[index] = 2;
-    } else if (strSplit[index] === 'i') {
-      strSplit[index] = 3;
-    } else if (strSplit[index] === 'o') {
-      strSplit[index] = 4;
-    } else if (strSplit[index] === 'u') {
-      strSplit[index] = 5;
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const numbers = ['1', '2', '3', '4', '5'];
+
+  let newStr = '';
+  for (letter of str) {
+    if (vowels.includes(letter)) {
+      newStr +=
+        numbers[vowels.indexOf(vowels.find((element) => element === letter))];
+    } else {
+      newStr += letter;
     }
   }
-  let encodedStr = strSplit.join('');
-  return encodedStr;
+
+  return newStr;
 }
 
 function decode(str) {
   // seu código aqui
-  let strSplit = str.split('');
-  for (let index in strSplit) {
-    if (strSplit[index] == 1) {
-      strSplit[index] = 'a';
-    } else if (strSplit[index] == 2) {
-      strSplit[index] = 'e';
-    } else if (strSplit[index] == 3) {
-      strSplit[index] = 'i';
-    } else if (strSplit[index] == 4) {
-      strSplit[index] = 'o';
-    } else if (strSplit[index] == 5) {
-      strSplit[index] = 'u';
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const numbers = ['1', '2', '3', '4', '5'];
+
+  let newStr = '';
+  for (letter of str) {
+    if (numbers.includes(letter)) {
+      newStr +=
+        vowels[numbers.indexOf(numbers.find((element) => element === letter))];
+    } else {
+      newStr += letter;
     }
   }
-  let decodedStr = strSplit.join('');
-  return decodedStr;
+
+  return newStr;
 }
 
-module.exports = { encode, decode };
+// module.exports = { encode, decode };
--- m1-fundamentos/b10-jest/exercicios/encode-decode.test.js
+++ m1-fundamentos/b10-jest/exercicios/encode-decode.test.js
@@ -14,4 +14,19 @@ describe('Exercício 4', () => {
   it('Para a função decode testa se os números 1, 2, 3, 4 e 5 são convertidos nas vogais a, e, i, o, u', () => {
     expect(encodeDecode.decode('1 2 3 4 5')).toMatch('a e i o u');
   });
+
+  it('Para a função encode, testa se as consoantes permanecem como consoantes', () => {
+    const consoates = 'b c d f g h j k l m n p q r s t v w x y z';
+    expect(encodeDecode.encode(consoates)).toMatch(
+      'b c d f g h j k l m n p q r s t v w x y z'
+    );
+  });
+
+  it('Para a função decode, testa se numeros acima de 5 permanecem os mesmos', () => {
+    expect(
+      encodeDecode
+        .decode('6 7 8 9 10 54 33 21 11')
+        .toMatch('6 7 8 9 10 54 33 21 11')
+    );
+  });
 });
