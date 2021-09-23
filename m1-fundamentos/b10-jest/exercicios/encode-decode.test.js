d871fb44b1f76954b7bef51c87bc591e8ff18f66
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-23 14:16:42 -0300

    [Exercicio 4.2] - Verifica se as vogais a, e, i, o, u sao convertidas em 1, 2, 3, 4, 5 para a funçao encode

--- m1-fundamentos/b10-jest/exercicios/encode-decode.test.js
+++ m1-fundamentos/b10-jest/exercicios/encode-decode.test.js
@@ -6,4 +6,8 @@ describe('Exercício 4', () => {
       'function'
     );
   });
+
+  it('Para a função encode testa se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5', () => {
+    expect(encodeDecode.encode('a e i o u')).toMatch('1 2 3 4 5');
+  });
 });
