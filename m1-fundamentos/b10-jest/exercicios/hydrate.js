c0e08eb00bcb661db966c80f109a71b38dc5d04e
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-23 15:40:05 -0300

    [Exercicio 6] - Adiciona comentario

--- m1-fundamentos/b10-jest/exercicios/hydrate.js
+++ m1-fundamentos/b10-jest/exercicios/hydrate.js
@@ -1,8 +1,13 @@
 function hydrate(str) {
   const regularExpression = /\d+/g;
+  // * \d --> metacharacter que busca por dígitos (números)
+  // No entanto, \d retorna a primeira ocorrência de numeros. Se tivermos uma string '1234_4321' vamos receber apenas '1234'
+  // * /g --> modificador 'global search'
+  // retorna todos os numeros na string
 
   const arrayNumbers = str
     .match(regularExpression)
+    // * match() --> método que recebe regular expressions e retorna os resultados
     .map((element) => parseInt(element));
   const sumWaterCups = arrayNumbers.reduce((acc, curVal) => acc + curVal);
 
