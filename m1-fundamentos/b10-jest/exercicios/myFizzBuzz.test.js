0027625aee4cf6c53b4f7c65aad0bc09783f754e
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-23 14:00:30 -0300

    [Exercicio 3.2] - Verifica se myFizzBuzz(${numero divisivel por 3}) retorna "fizz"

--- m1-fundamentos/b10-jest/exercicios/myFizzBuzz.test.js
+++ m1-fundamentos/b10-jest/exercicios/myFizzBuzz.test.js
@@ -4,4 +4,8 @@ describe('Exercício 3', () => {
   it('Verifica se myFizzBuzz(15) retorna "fizzbuzz"', () => {
     expect(myFizzBuzz(15)).toMatch('fizzbuzz');
   });
+
+  it('Verifica se myFizzBuzz(9) retorna "fizz"', () => {
+    expect(myFizzBuzz(9)).toMatch('fizz');
+  });
 });
