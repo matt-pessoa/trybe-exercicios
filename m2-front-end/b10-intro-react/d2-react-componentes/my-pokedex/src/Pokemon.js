c433d9b3d6359082470529bfc2e636c2fa2b00bf
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-10-07 17:12:15 -0300

    [Exercicios] - Corrige o item peso

--- m2-front-end/b10-intro-react/d2-react-componentes/my-pokedex/src/Pokemon.js
+++ m2-front-end/b10-intro-react/d2-react-componentes/my-pokedex/src/Pokemon.js
@@ -6,8 +6,10 @@ class Pokemon extends React.Component {
       <ul>
         <li>{this.props.pokemon.name}</li>
         <li>{this.props.pokemon.type}</li>
-        <li>{this.props.pokemon.averageWeight.value}</li>
-        <li>{this.props.pokemon.averageWeight.measurementUnit}</li>
+        <li>
+          {this.props.pokemon.averageWeight.value}{' '}
+          {this.props.pokemon.averageWeight.measurementUnit}
+        </li>
         <li>{this.props.pokemon.image}</li>
       </ul>
     );
