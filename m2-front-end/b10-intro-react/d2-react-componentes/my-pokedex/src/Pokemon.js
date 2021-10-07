e3e373c14e962614cc5acaed701b3d3d4cf5fe53
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-10-07 17:22:50 -0300

    [Exercicios] - Adiciona keys aos itens da lista

--- m2-front-end/b10-intro-react/d2-react-componentes/my-pokedex/src/Pokemon.js
+++ m2-front-end/b10-intro-react/d2-react-componentes/my-pokedex/src/Pokemon.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Pokemon extends React.Component {
   render() {
     return (
       <ul>
-        <li>{this.props.pokemon.name}</li>
-        <li>{this.props.pokemon.type}</li>
-        <li>
+        <li key='pokemon-name'>{this.props.pokemon.name}</li>
+        <li key='pokemon-type'>{this.props.pokemon.type}</li>
+        <li key='pokemon-weight'>
           {this.props.pokemon.averageWeight.value}{' '}
           {this.props.pokemon.averageWeight.measurementUnit}
         </li>
-        <li>{this.props.pokemon.image}</li>
+        <li key='pokemon-image'>{this.props.pokemon.image}</li>
       </ul>
     );
   }
 }
+
+Pokemon.propTypes = {
+  name: PropTypes.string,
+  type: PropTypes.string,
+  averageWeight: PropTypes.number,
+  measurementUnit: PropTypes.string,
+  image: PropTypes.string,
+};
 
 export default Pokemon;
