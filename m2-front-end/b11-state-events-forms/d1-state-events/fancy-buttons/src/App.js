c642c50e861b2cd1f860cb5023fc2d2095923ee0
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-10-18 20:33:38 -0300

    [Para fixar] this-bind - Declara tres funçoes e acessa o this dentro de cada uma. A primeira sem bind, a segunda com bind e a terceira uma arrow function

--- m2-front-end/b11-state-events-forms/d1-state-events/fancy-buttons/src/App.js
+++ m2-front-end/b11-state-events-forms/d1-state-events/fancy-buttons/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super();
+    // A função abaixo vincula "manualmente" o `this` à nossa função
     this.handleSecondButton = this.handleSecondButton.bind(this);
   }
 
@@ -12,12 +13,14 @@ class App extends React.Component {
   }
 
   handleSecondButton() {
-    return console.log(this); // é possível chamar o this neste caso, pois ele está ligado pelo constructor
+    /* Agora esse log retorna o objeto `this`, já acessível para nossa função!
+    Com isso, podemos acessar as `props`, estado do componente e tudo o mais daqui de dentro */
+    return console.log(this);
   }
 
-  handleThirdButton() {
-    return console.log('Third button');
-  }
+  handleThirdButton = () => {
+    return console.log(this);
+  };
   render() {
     return (
       <div className='App'>
