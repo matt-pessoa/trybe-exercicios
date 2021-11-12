0a736a5803f6d50232bab1d7eb530ad06fd430c5
matt-pessoa <matheuspessoafonseca@gmail.com>
2021-11-12 17:53:40 -0300

    Parte 1 - Exercicio 3

--- m2-front-end/b14-rtl/d2-mocks-inputs/exercises/part-1/service.test.js
+++ m2-front-end/b14-rtl/d2-mocks-inputs/exercises/part-1/service.test.js
@@ -1,8 +1,8 @@
-const { expect, it } = require("@jest/globals");
+const { expect, it, describe } = require("@jest/globals");
 const service = require("./service");
 
 describe("Testes para a função generateRandomNumber", () => {
-	it("Verifica se a função foi chamada", () => {
+	it("Exercício 1", () => {
 		service.generateRandomNumber = jest.fn().mockReturnValue(10);
 
 		expect(service.generateRandomNumber()).toBe(10);
@@ -10,7 +10,7 @@ describe("Testes para a função generateRandomNumber", () => {
 		expect(service.generateRandomNumber).toHaveBeenCalledTimes(1);
 	});
 
-	it("Verifica se a função foi chamada apenas uma vez", () => {
+	it("Exercício 2", () => {
 		service.generateRandomNumber = jest
 			.fn()
 			.mockImplementation((num1, num2) => {
@@ -19,6 +19,28 @@ describe("Testes para a função generateRandomNumber", () => {
 
 		expect(service.generateRandomNumber(10, 2)).toBe(5);
 		expect(service.generateRandomNumber).toHaveBeenCalled();
+		expect(service.generateRandomNumber).toHaveBeenCalledTimes(1);
+	});
+
+	it("Exercício 3", () => {
+		service.generateRandomNumber = jest
+			.fn()
+			.mockImplementation((num1, num2, num3) => {
+				return num1 * num2 * num3;
+			});
+
+		expect(service.generateRandomNumber(2, 2, 2)).toBe(8);
+
+		// Nova implementação
+
+		service.generateRandomNumber.mockReset();
+		expect(service.generateRandomNumber(2, 2, 2)).toBe(undefined);
+
+		service.generateRandomNumber = jest.fn().mockImplementation((num) => {
+			return num * 2;
+		});
+
+		expect(service.generateRandomNumber(2)).toBe(4);
 		expect(service.generateRandomNumber).toHaveBeenCalledTimes(1);
 	});
 });
