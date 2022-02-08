dd033a6b526ffe796e426cdb0f790a258749e0dd
matt-pessoa <matheuspessoafonseca@gmail.com>
2022-02-08 15:41:16 -0300

    Modifica a forma de valor recebido pelos inputs

--- m3-back-end/b22-node/d1-intro/exercicios/imc.js
+++ m3-back-end/b22-node/d1-intro/exercicios/imc.js
@@ -1,7 +1,7 @@
 const readline = require("readline-sync");
 
-const weight = readline.question("Qual o seu peso?");
-const height = readline.question("Qual a sua altura?");
+const weight = readline.questionFloat("Qual o seu peso?");
+const height = readline.questionFloat("Qual a sua altura?");
 
 function imcCalculator(weight, height) {
 	const imc = weight / height ** 2;
