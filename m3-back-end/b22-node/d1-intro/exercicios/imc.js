7616abae6e3b1e4d2513e1c357a23744c7a81518
matt-pessoa <matheuspessoafonseca@gmail.com>
2022-02-08 15:54:56 -0300

    Retorna tabela com o imc e a situaçao do peso

--- m3-back-end/b22-node/d1-intro/exercicios/imc.js
+++ m3-back-end/b22-node/d1-intro/exercicios/imc.js
@@ -3,9 +3,35 @@ const readline = require("readline-sync");
 const weight = readline.questionFloat("Qual o seu peso?");
 const height = readline.questionFloat("Qual a sua altura?");
 
-function imcCalculator(weight, height) {
-	const imc = weight / height ** 2;
-	return imc;
+function situation(imc) {
+	if (imc < 18.5) {
+		return "Abaixo do peso (magreza)";
+	}
+
+	if (imc >= 18.5 && imc < 25) {
+		return "Peso normal";
+	}
+
+	if (imc >= 25 && imc < 30) {
+		return "Acima do peso (sobrepeso)";
+	}
+
+	if (imc >= 30 && imc < 35) {
+		return "Obesidade grau I";
+	}
+
+	if (imc >= 35 && imc < 40) {
+		return "Obesidade grau II";
+	}
+
+	return "Obesidade graus III e IV";
 }
 
-console.log(imcCalculator(weight, height));
+function imcCalculator(weight, height) {
+	const imc = weight / height ** 2;
+	const table = [{ IMC: imc, Situação: situation(imc) }];
+
+	return table;
+}
+
+console.table(imcCalculator(weight, height));
