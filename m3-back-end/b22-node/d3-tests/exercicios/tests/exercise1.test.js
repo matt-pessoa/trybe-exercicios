a6a6046d65d1d6525d0cdd50a1f2a90296d945bd
matt-pessoa <matheuspessoafonseca@gmail.com>
2022-02-18 17:08:49 -0300

    [Exercicio 3] - Desenvolve o teste para o caso do valor recebido nao ser um numero

--- m3-back-end/b22-node/d3-tests/exercicios/tests/exercise1.test.js
+++ m3-back-end/b22-node/d3-tests/exercicios/tests/exercise1.test.js
@@ -1,23 +1,32 @@
 const { expect } = require("chai");
 const getSignal = require("../getSignal");
 
-describe("Quando o número for maior que zero", () => {
-	it("deve retornar positivo", () => {
-		const output = getSignal(5);
-		expect(output).to.be.equals("positivo");
+describe("O valor recebido é um número", () => {
+	describe("Quando o número for maior que zero", () => {
+		it("deve retornar positivo", () => {
+			const output = getSignal(5);
+			expect(output).to.be.equals("positivo");
+		});
+	});
+
+	describe("Quando o número for igual a zero", () => {
+		it("deve retornar neutro", () => {
+			const output = getSignal(0);
+			expect(output).to.be.equals("neutro");
+		});
+	});
+
+	describe("Quando o número for menor que zero", () => {
+		it("deve retornar negativo", () => {
+			const output = getSignal(-7);
+			expect(output).to.be.equals("negativo");
+		});
 	});
 });
 
-describe("Quando o número for igual a zero", () => {
-	it("deve retornar neutro", () => {
-		const output = getSignal(0);
-		expect(output).to.be.equals("neutro");
-	});
-});
-
-describe("Quando o número for menor que zero", () => {
-	it("deve retornar negativo", () => {
-		const output = getSignal(-7);
-		expect(output).to.be.equals("negativo");
+describe("O valor recebido não é um número", () => {
+	it("deve retornar um erro", () => {
+		const output = getSignal("string");
+		expect(output).to.be.equals("o valor deve ser um número");
 	});
 });
