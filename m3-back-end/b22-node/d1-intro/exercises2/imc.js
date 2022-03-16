b4a6d3d8efed8a8a435f9c824cba6ec59e60e833
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2022-03-16 13:35:28 -0300

    [Dia 1 - Node] - Exercicio 5: Indica a situaçao do peso da pessoa

--- m3-back-end/b22-node/d1-intro/exercises2/imc.js
+++ m3-back-end/b22-node/d1-intro/exercises2/imc.js
@@ -6,7 +6,22 @@ const imc = () => {
 
   const imcCalc = weight / height ** 2;
 
-  console.log(imcCalc.toFixed(2));
+  let situation;
+  if (imcCalc < 18.5) {
+    situation = 'Abaixo do peso';
+  } else if (imcCalc < 24.9) {
+    situation = 'Peso normal';
+  } else if (imcCalc < 29.9) {
+    situation = 'Sobrepeso';
+  } else if (imcCalc < 34.9) {
+    situation = 'Obesidade grau I';
+  } else if (imcCalc < 39.9) {
+    situation = 'Obesidade grau II';
+  } else {
+    situation = 'Obesidade grau III e IV';
+  }
+
+  console.log(situation);
 };
 
 imc();
